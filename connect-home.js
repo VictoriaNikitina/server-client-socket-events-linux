f2e1ae7679f803c6d47835e9f8dad57a131cebfa
Tory <71491518+nktntory@users.noreply.github.com>
2022-06-10 22:43:06 +0500

    Delete connect-home.js

--- connect-home.js
+++
@@ -1,9 +0,0 @@
-const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'BLR2_ARMS',
-    password: '12345678',
-    port: 5432   
-});
-module.exports = pool;
